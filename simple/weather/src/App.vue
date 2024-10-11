fa566970a94d022f76adbe4392fc699881b4ff6d
nforoutann <nazaninforoutan6@gmail.com>
2024-10-11 12:19:52 +0330

    feat: project ready to use

--- simple/weather/src/App.vue
+++ simple/weather/src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="hello">{{vari}}</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup(){
+    const vari = ref('hello')
+
+    return {vari}
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
